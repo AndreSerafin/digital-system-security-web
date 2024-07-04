7111e69be2d4ac755e6344e83e965915c4645ffe
Andre Pereira <andreserafim11@gmail.com>
2024-07-04 12:38:57 -0300

    feat: .env.example

--- src/app/(private)/systems/page.tsx
+++ src/app/(private)/systems/page.tsx
@@ -7,26 +7,21 @@ import { PermissionGate } from '@/components/permission-gate'
 
 export default function Systems() {
   return (
-    <PermissionGate
-      type="route"
-      allowedRoles={['SUPER_ADMIN', 'SYSTEM_ADMIN', 'TECHINICAL_MANAGER']}
-    >
-      <div className="flex flex-col gap-4 mt-4 mx-6">
-        <div className="flex justify-between">
-          <h1 className="text-3xl font-bold text-primary border-l-2 border-primary pl-4">
-            Pesquisar Sistema
-          </h1>
-          <PermissionGate type="route" allowedRoles={['SUPER_ADMIN']}>
-            <CreateSystemDialog />
-          </PermissionGate>
-        </div>
-        <Card>
-          <CardContent className="py-4 space-y-6">
-            <FilterForm />
-            <SystemsTable />
-          </CardContent>
-        </Card>
+    <div className="flex flex-col gap-4 mt-4 mx-6">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold text-primary border-l-2 border-primary pl-4">
+          Pesquisar Sistema
+        </h1>
+        <PermissionGate type="component" allowedRoles={['SUPER_ADMIN']}>
+          <CreateSystemDialog />
+        </PermissionGate>
       </div>
-    </PermissionGate>
+      <Card>
+        <CardContent className="py-4 space-y-6">
+          <FilterForm />
+          <SystemsTable />
+        </CardContent>
+      </Card>
+    </div>
   )
 }
